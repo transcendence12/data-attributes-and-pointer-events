92fa1ef9b8e5b5aa6c5148317aa2e9289e423b6b
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2022-10-04 13:00:53 +0200

    added code js: chcecking data attributes in the context of properties of event pointer

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 document.addEventListener('click', (e) => {
-    
+    // console.log(e) => Pointer event
+    // console.log(e.target.dataset.heart) click event outside icon =>undefined
+    // you need to check if target is icon itself
+    if(e.target.dataset.heart) {
+        console.log(e.target.dataset.heart)
+    } else if (e.target.dataset.share) {
+        console.log(e.target.dataset.share)
+    }
 })
